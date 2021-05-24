a6cea371536f41bc3543f6b8776941aa6c5ebd6b
martin <alvarez.martinjr@gmail.com>
2021-05-24 14:57:17 -0700

    fetched data from openweathermap

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// THIS IS TO MAKE MODAL WORK MANUALLY
 $('#trigger').click(function () {
 	$('#locModal').modal({show : true});
 });
@@ -15,4 +15,19 @@ closeButton.click( function (){
     modalShown.modal('hide')
 })
 
+// Init weather object
+const weather  = new Weather('Seattle','us','wa');
 
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather)
+
+// Calling the getWeather function will return a promise so we have to use .then and .catch
+function getWeather(){
+    weather.getWeather()
+    .then(results => {
+        console.log(results)
+    })
+    .catch( err => console.log(err));
+}
+
+    
